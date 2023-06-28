65073358c2858b788e40c41a3d32897155e0ae0f
cindy9899 <yeonhoo9899@gmail.com>
2023-06-28 11:06:29 +0900

    init

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -20,154 +20,154 @@ import { SessionProvider } from 'next-auth/react'
 
 export default function App({ Component, pageProps: {session, ...pageProps} }) {
     // jquery
-    // useEffect(() => {
-    //     $(function () {
-    //         //gnb 설정
+    useEffect(() => {
+        $(function () {
+            //gnb 설정
 
-    //         popupInit();
-    //         gnbInit();
-    //     });
+            popupInit();
+            gnbInit();
+        });
 
-    //     function popupInit() {
-    //         //alert  닫기팝업
-    //         $('.alert_popup .btn_close').on('click', function () {
-    //             $('#alert_mask')
-    //                 .stop(true, true)
-    //                 .animate({ opacity: 0 }, 200, function () {
-    //                     $(this).hide();
-    //                     // $(".t_btn").removeClass('on');
-    //                 });
-    //             $('.alert_popup:visible').stop(true, true).fadeOut(200);
-    //             return false;
-    //         });
-    //     }
+        function popupInit() {
+            //alert  닫기팝업
+            $('.alert_popup .btn_close').on('click', function () {
+                $('#alert_mask')
+                    .stop(true, true)
+                    .animate({ opacity: 0 }, 200, function () {
+                        $(this).hide();
+                        // $(".t_btn").removeClass('on');
+                    });
+                $('.alert_popup:visible').stop(true, true).fadeOut(200);
+                return false;
+            });
+        }
 
-    //     /*아코디언메뉴*/
-    //     $('.notice_listc').click(function () {
-    //         $('.notice_content').slideUp();
-    //         if ($(this).children('.notice_content').is(':hidden')) {
-    //             $(this).children('.notice_content').slideDown();
-    //         } else {
-    //             $(this).children('.notice_content').slideUp();
-    //         }
-    //     });
+        /*아코디언메뉴*/
+        $('.notice_listc').click(function () {
+            $('.notice_content').slideUp();
+            if ($(this).children('.notice_content').is(':hidden')) {
+                $(this).children('.notice_content').slideDown();
+            } else {
+                $(this).children('.notice_content').slideUp();
+            }
+        });
 
-    //     /*페이지 탑메뉴*/
-    //     function gnbInit() {
-    //         $('.page_navs .page_bsub').hover(
-    //             function () {
-    //                 $(this)
-    //                     .children('.psub_menu')
-    //                     .stop(true, true)
-    //                     .slideDown(200);
-    //             },
-    //             function () {
-    //                 $(this)
-    //                     .children('.psub_menu')
-    //                     .stop(true, true)
-    //                     .slideUp(200);
-    //             },
-    //         );
+        /*페이지 탑메뉴*/
+        function gnbInit() {
+            $('.page_navs .page_bsub').hover(
+                function () {
+                    $(this)
+                        .children('.psub_menu')
+                        .stop(true, true)
+                        .slideDown(200);
+                },
+                function () {
+                    $(this)
+                        .children('.psub_menu')
+                        .stop(true, true)
+                        .slideUp(200);
+                },
+            );
 
-    //         /*모바일*/
-    //         $('.page_navs .page_ssub').hover(
-    //             function () {
-    //                 $(this)
-    //                     .children('.psub_menub')
-    //                     .stop(true, true)
-    //                     .slideDown(200);
-    //             },
-    //             function () {
-    //                 $(this)
-    //                     .children('.psub_menub')
-    //                     .stop(true, true)
-    //                     .slideUp(200);
-    //             },
-    //         );
+            /*모바일*/
+            $('.page_navs .page_ssub').hover(
+                function () {
+                    $(this)
+                        .children('.psub_menub')
+                        .stop(true, true)
+                        .slideDown(200);
+                },
+                function () {
+                    $(this)
+                        .children('.psub_menub')
+                        .stop(true, true)
+                        .slideUp(200);
+                },
+            );
 
-    //         /*모바일*/
-    //         $('.page_navs .page_bsub').on('click', function () {
-    //             $('.psub_menu').toggleClass('active_1');
-    //         });
-    //     }
+            /*모바일*/
+            $('.page_navs .page_bsub').on('click', function () {
+                $('.psub_menu').toggleClass('active_1');
+            });
+        }
 
-    //     // var resizeTimeout;
+        // var resizeTimeout;
 
-    //     // clearTimeout(resizeTimeout);
-    //     // resizeTimeout = setTimeout(function () {
-    //     //     $(window)
-    //     //         .resize(function () {
-    //     //             if ($(window).width() > 993) {
-    //     //                 $('.nav-menu').addClass('gnbn');
-    //     //                 $('.nav-menu .gnb > li').addClass('gnb_memu');
-    //     //                 $('.nav-menu').removeClass('mobile_gnb');
-    //     //                 $('.nav-menu .gnb > li').removeClass('moblie_nav');
-    //     //             } else {
-    //     //                 $('.nav-menu').removeClass('gnbn');
-    //     //                 $('.nav-menu .gnb > li').removeClass('gnb_memu');
-    //     //                 $('.nav-menu').addClass('mobile_gnb');
-    //     //                 $('.nav-menu .gnb > li').addClass('moblie_nav');
-    //     //             }
-    //     //             if ($(window).width() > 993) {
-    //     //                 $('.gnbn > ul> li > a').on(
-    //     //                     'mouseover focus',
-    //     //                     function () {
-    //     //                         $(this)
-    //     //                             .parent()
-    //     //                             .addClass('active')
-    //     //                             .siblings()
-    //     //                             .removeClass('active');
-    //     //                         return false;
-    //     //                     },
-    //     //                 );
+        // clearTimeout(resizeTimeout);
+        // resizeTimeout = setTimeout(function () {
+        //     $(window)
+        //         .resize(function () {
+        //             if ($(window).width() > 993) {
+        //                 $('.nav-menu').addClass('gnbn');
+        //                 $('.nav-menu .gnb > li').addClass('gnb_memu');
+        //                 $('.nav-menu').removeClass('mobile_gnb');
+        //                 $('.nav-menu .gnb > li').removeClass('moblie_nav');
+        //             } else {
+        //                 $('.nav-menu').removeClass('gnbn');
+        //                 $('.nav-menu .gnb > li').removeClass('gnb_memu');
+        //                 $('.nav-menu').addClass('mobile_gnb');
+        //                 $('.nav-menu .gnb > li').addClass('moblie_nav');
+        //             }
+        //             if ($(window).width() > 993) {
+        //                 $('.gnbn > ul> li > a').on(
+        //                     'mouseover focus',
+        //                     function () {
+        //                         $(this)
+        //                             .parent()
+        //                             .addClass('active')
+        //                             .siblings()
+        //                             .removeClass('active');
+        //                         return false;
+        //                     },
+        //                 );
 
-    //     //                 $('.gnbn li li > a').on('mouseover focus', function () {
-    //     //                     $(this)
-    //     //                         .parent()
-    //     //                         .addClass('active_sub')
-    //     //                         .siblings()
-    //     //                         .removeClass('active_sub');
-    //     //                     return false;
-    //     //                 });
+        //                 $('.gnbn li li > a').on('mouseover focus', function () {
+        //                     $(this)
+        //                         .parent()
+        //                         .addClass('active_sub')
+        //                         .siblings()
+        //                         .removeClass('active_sub');
+        //                     return false;
+        //                 });
 
-    //     //                 if ($('.nav-menu.gnbn').is(':hidden')) {
-    //     //                     $(this).css('display', 'block');
-    //     //                 } else {
-    //     //                 }
-    //     //             } else {
-    //     //                 if ($('.nav-menu.gnbn').is(':hidden')) {
-    //     //                     $(this).css('display', 'block');
-    //     //                 } else {
-    //     //                 }
-    //     //                 // 햄버거메뉴
-    //     //                 $('.menu-trigger').on('click', function () {
-    //     //                     if ($('.mobile_gnb').is(':hidden')) {
-    //     //                         $('.mobile_gnb').stop(true, true).fadeIn(200);
-    //     //                         $('.menu-trigger').addClass('active-1');
+        //                 if ($('.nav-menu.gnbn').is(':hidden')) {
+        //                     $(this).css('display', 'block');
+        //                 } else {
+        //                 }
+        //             } else {
+        //                 if ($('.nav-menu.gnbn').is(':hidden')) {
+        //                     $(this).css('display', 'block');
+        //                 } else {
+        //                 }
+        //                 // 햄버거메뉴
+        //                 $('.menu-trigger').on('click', function () {
+        //                     if ($('.mobile_gnb').is(':hidden')) {
+        //                         $('.mobile_gnb').stop(true, true).fadeIn(200);
+        //                         $('.menu-trigger').addClass('active-1');
 
-    //     //                         $('.moblie_nav').click(function () {
-    //     //                             $(this).children('.inner').slideUp();
-    //     //                             if (
-    //     //                                 $(this).children('.inner').is(':hidden')
-    //     //                             ) {
-    //     //                                 $(this).children('.inner').slideDown();
-    //     //                             } else {
-    //     //                                 $(this).children('.inner').slideUp();
-    //     //                             }
-    //     //                         });
-    //     //                     } else {
-    //     //                         $('.mobile_gnb').stop(true, true).fadeOut(200);
-    //     //                         $('.menu-trigger').removeClass('active-1');
-    //     //                     }
-    //     //                     return false;
-    //     //                 });
+        //                         $('.moblie_nav').click(function () {
+        //                             $(this).children('.inner').slideUp();
+        //                             if (
+        //                                 $(this).children('.inner').is(':hidden')
+        //                             ) {
+        //                                 $(this).children('.inner').slideDown();
+        //                             } else {
+        //                                 $(this).children('.inner').slideUp();
+        //                             }
+        //                         });
+        //                     } else {
+        //                         $('.mobile_gnb').stop(true, true).fadeOut(200);
+        //                         $('.menu-trigger').removeClass('active-1');
+        //                     }
+        //                     return false;
+        //                 });
 
-    //     //                 // return false;
-    //     //             }
-    //     //         })
-    //     //         .resize();
-    //     // }, 10);
-    // }, [pageProps]);
+        //                 // return false;
+        //             }
+        //         })
+        //         .resize();
+        // }, 10);
+    }, [pageProps]);
 
     // 팝업창
     function alert_show(target) {
@@ -186,23 +186,8 @@ export default function App({ Component, pageProps: {session, ...pageProps} }) {
 
     return (
         <>
-            <Head>
-                <meta charSet="utf-8" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1, shrink-to-fit=no"
-                />
-                <meta name="description" content="Red Account" />
-                <meta name="author" content="" />
-                <meta name="generator" content="" />
-                <meta
-                    httpEquiv="Content-Security-Policy"
-                    content="upgrade-insecure-requests"
-                />
-                <link rel="icon" href="/favicon.ico" sizes="any" />
-            </Head>
             <SessionProvider session={session}>
-            <Component {...pageProps} alert_show={alert_show} />
+                <Component {...pageProps} alert_show={alert_show} />
             </SessionProvider>
         </>
     );
